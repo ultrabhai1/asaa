cc2461fd90d5fb450317d3b588bfade48684d3c7
ultrabhai1 <ranjhaediting681@gmail.com>
2026-05-02 19:44:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "Compiling C flood tool..."
-gcc -O3 -pthread -o udp_flood udp_flood.c
-if [ $? -eq 0 ]; then
-    echo "✅ Compilation success"
-else
-    echo "❌ Compilation failed. Install gcc: pkg install gcc"
-    exit 1
-fi
+
+echo "Starting binary..."
+
+# Permission do
+chmod +x bgmi
+
+# Run karo (agar background me chalana hai)
+./bgmi &
 
 echo "Installing Python requirements..."
 pip install pyTelegramBotAPI
